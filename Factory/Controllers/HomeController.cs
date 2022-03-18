f244321c928bb8841a4c536af7b1822766b1a5d1
Chase Stokes <chasehunterstokes@gmail.com>
2022-03-18 11:59:36 -0700

    WIP displaying workers and machines on splash

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,14 +1,31 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Factory.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
 
       [HttpGet("/")]
       public ActionResult Index()
       {
-        return View();
+        {
+      var workers =_db.Workers.ToList();
+      var machines =_db.Machines.ToList();
+      ViewBag.Engineers = workers;
+      ViewBag.Machines = machines;
+      return View();
+    }
       }
 
     }
